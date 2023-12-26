f28f6e981a1e0a70030a1e456eba989ab7e821ce
jaqvillorejo <154889893+jaqvillorejo@users.noreply.github.com>
2023-12-26 20:55:33 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,155 @@
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+
+const Flashlight = ({ isOn, toggleFlashlight, goBack }) => {
+  return (
+    <View style={styles.center}>
+      <Text style={styles.title}>Flashlight is {isOn ? "On" : "Off"}</Text>
+      <View style={styles.imageContainer}>
+        <Image
+          source={{
+            uri: isOn
+              ? "https://image.freepik.com/free-icon/flashlight_318-47582.jpg"
+              : "https://th.bing.com/th/id/OIP.wFxg0wKKtEp6X2rMsdZoKwHaHZ?pid=ImgDet&rs=1",
+          }}
+          style={styles.image}
+        />
+      </View>
+      <Button
+        title={isOn ? "Turn Off" : "Turn On"}
+        onPress={toggleFlashlight}
+        color={isOn ? "red" : "green"}
+      />
+      <TouchableOpacity style={styles.backButton} onPress={goBack}>
+        <Text style={styles.backButtonText}>Back</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const Counter = ({ goBack }) => {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <View style={styles.center}>
+      <Text style={styles.counterText}> {count}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="+1" onPress={incrementCount} />
+        <Button title="-1" onPress={decrementCount} />
+      </View>
+      <TouchableOpacity style={styles.backButton} onPress={goBack}>
+        <Text style={styles.backButtonText}>Back</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const Main = () => {
+  const [flights, setFlight] = useState(false);
+  const [counter, setCounter] = useState(true);
+  const [flashlightOn, setFlashlightOn] = useState(false);
+
+  const toflight = () => {
+    setFlight(true);
+    setCounter(true);
+    setFlashlightOn(!flashlightOn);
+  };
+
+  const toCounter = () => {
+    setCounter(false);
+    setFlight(false);
+    setFlashlightOn(!flashlightOn);
+    setFlashlightOff(!setFlashlightOff);
+  };
+
+  const toggleFlashlight = () => {
+    setFlashlightOn(!flashlightOn);
+    setCounter(true);
+  };
+
+  const goBack = () => {
+    setFlight(false);
+    setCounter(true);
+    setFlashlightOn(false);
+  };
+
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="FLASHLIGHT"
+          onPress={toflight}
+          disabled={flights || flashlightOn}
+        />
+        <Button
+          title="COUNTER"
+          onPress={toCounter}
+          disabled={flights  || !counter}
+        />
+      </View>
+      {flights && <Flashlight isOn={flashlightOn} toggleFlashlight={toggleFlashlight} goBack={goBack} />}
+      {!counter && <Counter goBack={goBack} />}
+    </View>
+  );
+};
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Main />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingVertical: 20,
+  },
+  mainContainer: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  center: {
+    alignItems: "center",
+  },
+  imageContainer: {
+    alignItems: "center",
+  },
+  image: {
+    width: 300,
+    height: 300,
+    marginVertical: 20,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+  },
+  backButton: {
+    marginTop: 20,
+  },
+  backButtonText: {
+    fontSize: 18,
+    color: "",
+  },
+  counterText: {
+    fontSize: 200, 
+    marginBottom: 20,
+  },
+});
